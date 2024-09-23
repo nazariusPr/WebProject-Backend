567a61a14f551874fe57543147dfebb597ddb1e5
nazariusPr <protsnazar2004@gmail.com>
2024-09-23 20:28:09 +0300

    configure cors

--- src/main/java/com/nazarois/WebProject/util/ClientHelper.java
+++ src/main/java/com/nazarois/WebProject/util/ClientHelper.java
@@ -3,10 +3,12 @@ package com.nazarois.WebProject.util;
 public class ClientHelper {
   private ClientHelper() {}
 
-  private static final String CLIENT_URL = "http://localhost:9090";
+  private static final String SERVER_URL = "http://localhost:9090";
   private static final String VERIFY_EMAIL_URL_FORMAT = "%s/verify?token=%s";
 
+  public static final String CLIENT_URL = "http://localhost:9091";
+
   public static String getVerificationUrl(String token) {
-    return String.format(VERIFY_EMAIL_URL_FORMAT, CLIENT_URL, token);
+    return String.format(VERIFY_EMAIL_URL_FORMAT, SERVER_URL, token);
   }
 }
