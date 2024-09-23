6bcbc95e613703f204bdea2028e5b3d7c8adf432
nazariusPr <115506158+nazariusPr@users.noreply.github.com>
2024-09-23 20:28:44 +0300

    configure cors (#5)

--- src/main/java/com/nazarois/WebProject/config/SecurityConfig.java
+++ src/main/java/com/nazarois/WebProject/config/SecurityConfig.java
@@ -1,8 +1,11 @@
 package com.nazarois.WebProject.config;
 
 import static com.nazarois.WebProject.constants.AppConstants.AUTH_LINK;
+import static com.nazarois.WebProject.util.ClientHelper.CLIENT_URL;
 
 import com.nazarois.WebProject.security.filter.JwtAuthenticationFilter;
+import java.util.Arrays;
+import java.util.List;
 import lombok.AllArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -14,6 +17,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
 
 @Configuration
 @AllArgsConstructor
@@ -25,7 +29,28 @@ public class SecurityConfig {
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
+    http.cors(
+            corsCustomizer ->
+                corsCustomizer.configurationSource(
+                    request -> {
+                      CorsConfiguration config = new CorsConfiguration();
+                      config.setAllowedOriginPatterns(List.of(CLIENT_URL));
+                      config.setAllowedMethods(
+                          Arrays.asList("GET", "POST", "OPTIONS", "DELETE", "PUT", "PATCH"));
+                      config.setAllowedHeaders(
+                          Arrays.asList(
+                              "Access-Control-Allow-Origin",
+                              "Access-Control-Allow-Headers",
+                              "X-Requested-With",
+                              "Origin",
+                              "Content-Type",
+                              "Accept",
+                              "Authorization"));
+                      config.setAllowCredentials(true);
+                      config.setMaxAge(3600L);
+                      return config;
+                    }))
+        .csrf(AbstractHttpConfigurer::disable)
         .authorizeHttpRequests(auth -> auth.requestMatchers(AUTH_LINK + "/**").permitAll())
         .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authenticationProvider(this.authenticationProvider)
